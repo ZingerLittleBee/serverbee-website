188a31bd59c3d6d647a4c1f92d355c7d3db5d634
ZingerLittleBee <6970999@gmail.com>
2023-03-02 04:30:14 +0800

    init: 🎉 docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import preact from '@astrojs/preact'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()]
+	integrations: [tailwind(), react(), preact()],
+	site: `https://serverbee.app`
 })
