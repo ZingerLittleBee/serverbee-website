788c6731e3644a5e465d77a79c345c2393ac0ec5
ZingerLittleBee <6970999@gmail.com>
2023-03-09 00:30:00 +0800

    feat: 🎸 toc

--- src/components/RightSidebar/TableOfContents.tsx
+++ src/components/RightSidebar/TableOfContents.tsx
@@ -83,7 +83,9 @@ const TableOfContents: FunctionalComponent<{ headings: MarkdownHeading[] }> = ({
 					.filter(({ depth }) => depth > 1 && depth < 4)
 					.map(heading => (
 						<li
-							className={`header-link depth-${heading.depth} ${
+							className={`header-link 
+							text-gray-500
+							depth-${heading.depth} ${
 								currentID === heading.slug
 									? 'current-header-link'
 									: ''
