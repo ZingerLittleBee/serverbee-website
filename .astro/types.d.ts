f32152f1df1d85eac5abd148f4efb16ff5eb3b04
ZingerLittleBee <6970999@gmail.com>
2023-05-06 01:02:26 +0800

    fix: 🐛 build error

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,7 +1,64 @@
 declare module 'astro:content' {
+	interface Render {
+		'.mdx': Promise<{
+			Content: import('astro').MarkdownInstance<{}>['Content'];
+			headings: import('astro').MarkdownHeading[];
+			remarkPluginFrontmatter: Record<string, any>;
+		}>;
+	}
+}
+
+declare module 'astro:content' {
+	interface Render {
+		'.md': Promise<{
+			Content: import('astro').MarkdownInstance<{}>['Content'];
+			headings: import('astro').MarkdownHeading[];
+			remarkPluginFrontmatter: Record<string, any>;
+		}>;
+	}
+}
+
+declare module 'astro:content' {
 	export { z } from 'astro/zod';
 	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]] & Render;
+		(typeof entryMap)[C][keyof (typeof entryMap)[C]];
+
+	// TODO: Remove this when having this fallback is no longer relevant. 2.3? 3.0? - erika, 2023-04-04
+	/**
+	 * @deprecated
+	 * `astro:content` no longer provide `image()`.
+	 *
+	 * Please use it through `schema`, like such:
+	 * ```ts
+	 * import { defineCollection, z } from "astro:content";
+	 *
+	 * defineCollection({
+	 *   schema: ({ image }) =>
+	 *     z.object({
+	 *       image: image(),
+	 *     }),
+	 * });
+	 * ```
+	 */
+	export const image: never;
+
+	// This needs to be in sync with ImageMetadata
+	export type ImageFunction = () => import('astro/zod').ZodObject<{
+		src: import('astro/zod').ZodString;
+		width: import('astro/zod').ZodNumber;
+		height: import('astro/zod').ZodNumber;
+		format: import('astro/zod').ZodUnion<
+			[
+				import('astro/zod').ZodLiteral<'png'>,
+				import('astro/zod').ZodLiteral<'jpg'>,
+				import('astro/zod').ZodLiteral<'jpeg'>,
+				import('astro/zod').ZodLiteral<'tiff'>,
+				import('astro/zod').ZodLiteral<'webp'>,
+				import('astro/zod').ZodLiteral<'gif'>,
+				import('astro/zod').ZodLiteral<'svg'>
+			]
+		>;
+	}>;
 
 	type BaseSchemaWithoutEffects =
 		| import('astro/zod').AnyZodObject
@@ -16,15 +73,10 @@ declare module 'astro:content' {
 		| BaseSchemaWithoutEffects
 		| import('astro/zod').ZodEffects<BaseSchemaWithoutEffects>;
 
+	export type SchemaContext = { image: ImageFunction };
+
 	type BaseCollectionConfig<S extends BaseSchema> = {
-		schema?: S;
-		slug?: (entry: {
-			id: CollectionEntry<keyof typeof entryMap>['id'];
-			defaultSlug: string;
-			collection: string;
-			body: string;
-			data: import('astro/zod').infer<S>;
-		}) => string | Promise<string>;
+		schema?: S | ((context: SchemaContext) => S);
 	};
 	export function defineCollection<S extends BaseSchema>(
 		input: BaseCollectionConfig<S>
@@ -53,17 +105,10 @@ declare module 'astro:content' {
 		filter?: (entry: CollectionEntry<C>) => unknown
 	): Promise<CollectionEntry<C>[]>;
 
+	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
 	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
-		Required<ContentConfig['collections'][C]>['schema']
+		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
-
-	type Render = {
-		render(): Promise<{
-			Content: import('astro').MarkdownInstance<{}>['Content'];
-			headings: import('astro').MarkdownHeading[];
-			remarkPluginFrontmatter: Record<string, any>;
-		}>;
-	};
 
 	const entryMap: {
 		"docs": {
@@ -73,252 +118,252 @@ declare module 'astro:content' {
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/future/requirement-wall.md": {
   id: "en/future/requirement-wall.md",
   slug: "en/future/requirement-wall",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/future/roadmap.md": {
   id: "en/future/roadmap.md",
   slug: "en/future/roadmap",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/policy/privacy.md": {
   id: "en/policy/privacy.md",
   slug: "en/policy/privacy",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/question/faq.md": {
   id: "en/question/faq.md",
   slug: "en/question/faq",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/question/known-problem.md": {
   id: "en/question/known-problem.md",
   slug: "en/question/known-problem",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/starter/changelog.md": {
   id: "en/starter/changelog.md",
   slug: "en/starter/changelog",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/starter/deploy.mdx": {
   id: "en/starter/deploy.mdx",
   slug: "en/starter/deploy",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/starter/getting-started.md": {
   id: "en/starter/getting-started.md",
   slug: "en/starter/getting-started",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/starter/module.md": {
   id: "en/starter/module.md",
   slug: "en/starter/module",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/starter/web.mdx": {
   id: "en/starter/web.mdx",
   slug: "en/starter/web",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/README.md": {
   id: "en/usage/README.md",
   slug: "en/usage/readme",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "en/usage/docker.mdx": {
   id: "en/usage/docker.mdx",
   slug: "en/usage/docker",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/linux.mdx": {
   id: "en/usage/linux.mdx",
   slug: "en/usage/linux",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/macos.mdx": {
   id: "en/usage/macos.mdx",
   slug: "en/usage/macos",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/qts.mdx": {
   id: "en/usage/qts.mdx",
   slug: "en/usage/qts",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/synology.mdx": {
   id: "en/usage/synology.mdx",
   slug: "en/usage/synology",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "en/usage/windows.mdx": {
   id: "en/usage/windows.mdx",
   slug: "en/usage/windows",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/contact/way.md": {
   id: "zh/contact/way.md",
   slug: "zh/contact/way",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/future/requirement-wall.md": {
   id: "zh/future/requirement-wall.md",
   slug: "zh/future/requirement-wall",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/future/roadmap.md": {
   id: "zh/future/roadmap.md",
   slug: "zh/future/roadmap",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/policy/privacy.md": {
   id: "zh/policy/privacy.md",
   slug: "zh/policy/privacy",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/question/faq.md": {
   id: "zh/question/faq.md",
   slug: "zh/question/faq",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/question/known-problem.md": {
   id: "zh/question/known-problem.md",
   slug: "zh/question/known-problem",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/starter/changelog.md": {
   id: "zh/starter/changelog.md",
   slug: "zh/starter/changelog",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/starter/deploy.mdx": {
   id: "zh/starter/deploy.mdx",
   slug: "zh/starter/deploy",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/starter/getting-started.md": {
   id: "zh/starter/getting-started.md",
   slug: "zh/starter/getting-started",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/starter/module.md": {
   id: "zh/starter/module.md",
   slug: "zh/starter/module",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/starter/web.mdx": {
   id: "zh/starter/web.mdx",
   slug: "zh/starter/web",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/README.md": {
   id: "zh/usage/README.md",
   slug: "zh/usage/readme",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".md"] },
 "zh/usage/docker.mdx": {
   id: "zh/usage/docker.mdx",
   slug: "zh/usage/docker",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/linux.mdx": {
   id: "zh/usage/linux.mdx",
   slug: "zh/usage/linux",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/macos.mdx": {
   id: "zh/usage/macos.mdx",
   slug: "zh/usage/macos",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/qts.mdx": {
   id: "zh/usage/qts.mdx",
   slug: "zh/usage/qts",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/synology.mdx": {
   id: "zh/usage/synology.mdx",
   slug: "zh/usage/synology",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 "zh/usage/windows.mdx": {
   id: "zh/usage/windows.mdx",
   slug: "zh/usage/windows",
   body: string,
   collection: "docs",
   data: InferEntrySchema<"docs">
-},
+} & { render(): Render[".mdx"] },
 },
 
 	};
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react(), preact(), mdx()],
-	site: `https://serverbee.app`
+	site: `https://serverbee.app/`
 })
