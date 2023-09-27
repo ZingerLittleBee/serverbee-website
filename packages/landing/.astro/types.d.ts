64e6db7e09543ce59922ea74adaecf7fb2fd0e24
ZingerLittleBee <6970999@gmail.com>
2023-09-27 19:53:34 +0800

    fix: 🐛 lock version

--- packages/landing/.astro/types.d.ts
+++ packages/landing/.astro/types.d.ts
@@ -20,8 +20,7 @@ declare module 'astro:content' {
 
 declare module 'astro:content' {
 	export { z } from 'astro/zod';
-	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]];
+	export type CollectionEntry<C extends keyof AnyEntryMap> = AnyEntryMap[C][keyof AnyEntryMap[C]];
 
 	// TODO: Remove this when having this fallback is no longer relevant. 2.3? 3.0? - erika, 2023-04-04
 	/**
@@ -75,298 +74,392 @@ declare module 'astro:content' {
 
 	export type SchemaContext = { image: ImageFunction };
 
-	type BaseCollectionConfig<S extends BaseSchema> = {
+	type DataCollectionConfig<S extends BaseSchema> = {
+		type: 'data';
 		schema?: S | ((context: SchemaContext) => S);
 	};
-	export function defineCollection<S extends BaseSchema>(
-		input: BaseCollectionConfig<S>
-	): BaseCollectionConfig<S>;
 
-	type EntryMapKeys = keyof typeof entryMap;
+	type ContentCollectionConfig<S extends BaseSchema> = {
+		type?: 'content';
+		schema?: S | ((context: SchemaContext) => S);
+	};
+
+	type CollectionConfig<S> = ContentCollectionConfig<S> | DataCollectionConfig<S>;
+
+	export function defineCollection<S extends BaseSchema>(
+		input: CollectionConfig<S>
+	): CollectionConfig<S>;
+
 	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
-	type ValidEntrySlug<C extends EntryMapKeys> = AllValuesOf<(typeof entryMap)[C]>['slug'];
+	type ValidContentEntrySlug<C extends keyof ContentEntryMap> = AllValuesOf<
+		ContentEntryMap[C]
+	>['slug'];
 
 	export function getEntryBySlug<
-		C extends keyof typeof entryMap,
-		E extends ValidEntrySlug<C> | (string & {})
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
 	>(
 		collection: C,
 		// Note that this has to accept a regular string too, for SSR
 		entrySlug: E
-	): E extends ValidEntrySlug<C>
+	): E extends ValidContentEntrySlug<C>
 		? Promise<CollectionEntry<C>>
 		: Promise<CollectionEntry<C> | undefined>;
-	export function getCollection<C extends keyof typeof entryMap, E extends CollectionEntry<C>>(
+
+	export function getDataEntryById<C extends keyof DataEntryMap, E extends keyof DataEntryMap[C]>(
+		collection: C,
+		entryId: E
+	): Promise<CollectionEntry<C>>;
+
+	export function getCollection<C extends keyof AnyEntryMap, E extends CollectionEntry<C>>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => entry is E
 	): Promise<E[]>;
-	export function getCollection<C extends keyof typeof entryMap>(
+	export function getCollection<C extends keyof AnyEntryMap>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => unknown
 	): Promise<CollectionEntry<C>[]>;
 
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(entry: {
+		collection: C;
+		slug: E;
+	}): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(entry: {
+		collection: C;
+		id: E;
+	}): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(
+		collection: C,
+		slug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(
+		collection: C,
+		id: E
+	): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	/** Resolve an array of entry references from the same collection */
+	export function getEntries<C extends keyof ContentEntryMap>(
+		entries: {
+			collection: C;
+			slug: ValidContentEntrySlug<C>;
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+	export function getEntries<C extends keyof DataEntryMap>(
+		entries: {
+			collection: C;
+			id: keyof DataEntryMap[C];
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+
+	export function reference<C extends keyof AnyEntryMap>(
+		collection: C
+	): import('astro/zod').ZodEffects<
+		import('astro/zod').ZodString,
+		C extends keyof ContentEntryMap
+			? {
+					collection: C;
+					slug: ValidContentEntrySlug<C>;
+			  }
+			: {
+					collection: C;
+					id: keyof DataEntryMap[C];
+			  }
+	>;
+	// Allow generic `string` to avoid excessive type errors in the config
+	// if `dev` is not running to update as you edit.
+	// Invalid collection names will be caught at build time.
+	export function reference<C extends string>(
+		collection: C
+	): import('astro/zod').ZodEffects<import('astro/zod').ZodString, never>;
+
 	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
-	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
+	type InferEntrySchema<C extends keyof AnyEntryMap> = import('astro/zod').infer<
 		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
 
-	const entryMap: {
+	type ContentEntryMap = {
 		"docs": {
 "en/contact/way.md": {
-  id: "en/contact/way.md",
-  slug: "en/contact/way",
-  body: string,
-  collection: "docs",
+	id: "en/contact/way.md";
+  slug: "en/contact/way";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/future/requirement-wall.md": {
-  id: "en/future/requirement-wall.md",
-  slug: "en/future/requirement-wall",
-  body: string,
-  collection: "docs",
+	id: "en/future/requirement-wall.md";
+  slug: "en/future/requirement-wall";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/future/roadmap.md": {
-  id: "en/future/roadmap.md",
-  slug: "en/future/roadmap",
-  body: string,
-  collection: "docs",
+	id: "en/future/roadmap.md";
+  slug: "en/future/roadmap";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/policy/privacy.md": {
-  id: "en/policy/privacy.md",
-  slug: "en/policy/privacy",
-  body: string,
-  collection: "docs",
+	id: "en/policy/privacy.md";
+  slug: "en/policy/privacy";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/question/faq.md": {
-  id: "en/question/faq.md",
-  slug: "en/question/faq",
-  body: string,
-  collection: "docs",
+	id: "en/question/faq.md";
+  slug: "en/question/faq";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/question/known-problem.md": {
-  id: "en/question/known-problem.md",
-  slug: "en/question/known-problem",
-  body: string,
-  collection: "docs",
+	id: "en/question/known-problem.md";
+  slug: "en/question/known-problem";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/starter/changelog.md": {
-  id: "en/starter/changelog.md",
-  slug: "en/starter/changelog",
-  body: string,
-  collection: "docs",
+	id: "en/starter/changelog.md";
+  slug: "en/starter/changelog";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/starter/deploy.mdx": {
-  id: "en/starter/deploy.mdx",
-  slug: "en/starter/deploy",
-  body: string,
-  collection: "docs",
+	id: "en/starter/deploy.mdx";
+  slug: "en/starter/deploy";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/starter/getting-started.md": {
-  id: "en/starter/getting-started.md",
-  slug: "en/starter/getting-started",
-  body: string,
-  collection: "docs",
+	id: "en/starter/getting-started.md";
+  slug: "en/starter/getting-started";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/starter/module.md": {
-  id: "en/starter/module.md",
-  slug: "en/starter/module",
-  body: string,
-  collection: "docs",
+	id: "en/starter/module.md";
+  slug: "en/starter/module";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/starter/web.mdx": {
-  id: "en/starter/web.mdx",
-  slug: "en/starter/web",
-  body: string,
-  collection: "docs",
+	id: "en/starter/web.mdx";
+  slug: "en/starter/web";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/README.md": {
-  id: "en/usage/README.md",
-  slug: "en/usage/readme",
-  body: string,
-  collection: "docs",
+	id: "en/usage/README.md";
+  slug: "en/usage/readme";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "en/usage/docker.mdx": {
-  id: "en/usage/docker.mdx",
-  slug: "en/usage/docker",
-  body: string,
-  collection: "docs",
+	id: "en/usage/docker.mdx";
+  slug: "en/usage/docker";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/linux.mdx": {
-  id: "en/usage/linux.mdx",
-  slug: "en/usage/linux",
-  body: string,
-  collection: "docs",
+	id: "en/usage/linux.mdx";
+  slug: "en/usage/linux";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/macos.mdx": {
-  id: "en/usage/macos.mdx",
-  slug: "en/usage/macos",
-  body: string,
-  collection: "docs",
+	id: "en/usage/macos.mdx";
+  slug: "en/usage/macos";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/qts.mdx": {
-  id: "en/usage/qts.mdx",
-  slug: "en/usage/qts",
-  body: string,
-  collection: "docs",
+	id: "en/usage/qts.mdx";
+  slug: "en/usage/qts";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/synology.mdx": {
-  id: "en/usage/synology.mdx",
-  slug: "en/usage/synology",
-  body: string,
-  collection: "docs",
+	id: "en/usage/synology.mdx";
+  slug: "en/usage/synology";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "en/usage/windows.mdx": {
-  id: "en/usage/windows.mdx",
-  slug: "en/usage/windows",
-  body: string,
-  collection: "docs",
+	id: "en/usage/windows.mdx";
+  slug: "en/usage/windows";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/contact/way.md": {
-  id: "zh/contact/way.md",
-  slug: "zh/contact/way",
-  body: string,
-  collection: "docs",
+	id: "zh/contact/way.md";
+  slug: "zh/contact/way";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/future/requirement-wall.md": {
-  id: "zh/future/requirement-wall.md",
-  slug: "zh/future/requirement-wall",
-  body: string,
-  collection: "docs",
+	id: "zh/future/requirement-wall.md";
+  slug: "zh/future/requirement-wall";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/future/roadmap.md": {
-  id: "zh/future/roadmap.md",
-  slug: "zh/future/roadmap",
-  body: string,
-  collection: "docs",
+	id: "zh/future/roadmap.md";
+  slug: "zh/future/roadmap";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/policy/privacy.md": {
-  id: "zh/policy/privacy.md",
-  slug: "zh/policy/privacy",
-  body: string,
-  collection: "docs",
+	id: "zh/policy/privacy.md";
+  slug: "zh/policy/privacy";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/question/faq.md": {
-  id: "zh/question/faq.md",
-  slug: "zh/question/faq",
-  body: string,
-  collection: "docs",
+	id: "zh/question/faq.md";
+  slug: "zh/question/faq";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/question/known-problem.md": {
-  id: "zh/question/known-problem.md",
-  slug: "zh/question/known-problem",
-  body: string,
-  collection: "docs",
+	id: "zh/question/known-problem.md";
+  slug: "zh/question/known-problem";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/starter/changelog.md": {
-  id: "zh/starter/changelog.md",
-  slug: "zh/starter/changelog",
-  body: string,
-  collection: "docs",
+	id: "zh/starter/changelog.md";
+  slug: "zh/starter/changelog";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/starter/deploy.mdx": {
-  id: "zh/starter/deploy.mdx",
-  slug: "zh/starter/deploy",
-  body: string,
-  collection: "docs",
+	id: "zh/starter/deploy.mdx";
+  slug: "zh/starter/deploy";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/starter/getting-started.md": {
-  id: "zh/starter/getting-started.md",
-  slug: "zh/starter/getting-started",
-  body: string,
-  collection: "docs",
+	id: "zh/starter/getting-started.md";
+  slug: "zh/starter/getting-started";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/starter/module.md": {
-  id: "zh/starter/module.md",
-  slug: "zh/starter/module",
-  body: string,
-  collection: "docs",
+	id: "zh/starter/module.md";
+  slug: "zh/starter/module";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/starter/web.mdx": {
-  id: "zh/starter/web.mdx",
-  slug: "zh/starter/web",
-  body: string,
-  collection: "docs",
+	id: "zh/starter/web.mdx";
+  slug: "zh/starter/web";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/README.md": {
-  id: "zh/usage/README.md",
-  slug: "zh/usage/readme",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/README.md";
+  slug: "zh/usage/readme";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "zh/usage/docker.mdx": {
-  id: "zh/usage/docker.mdx",
-  slug: "zh/usage/docker",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/docker.mdx";
+  slug: "zh/usage/docker";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/linux.mdx": {
-  id: "zh/usage/linux.mdx",
-  slug: "zh/usage/linux",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/linux.mdx";
+  slug: "zh/usage/linux";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/macos.mdx": {
-  id: "zh/usage/macos.mdx",
-  slug: "zh/usage/macos",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/macos.mdx";
+  slug: "zh/usage/macos";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/qts.mdx": {
-  id: "zh/usage/qts.mdx",
-  slug: "zh/usage/qts",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/qts.mdx";
+  slug: "zh/usage/qts";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/synology.mdx": {
-  id: "zh/usage/synology.mdx",
-  slug: "zh/usage/synology",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/synology.mdx";
+  slug: "zh/usage/synology";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "zh/usage/windows.mdx": {
-  id: "zh/usage/windows.mdx",
-  slug: "zh/usage/windows",
-  body: string,
-  collection: "docs",
+	id: "zh/usage/windows.mdx";
+  slug: "zh/usage/windows";
+  body: string;
+  collection: "docs";
   data: InferEntrySchema<"docs">
-} & { render(): Render[".mdx"] },
-},
+} & { render(): Render[".mdx"] };
+};
 
 	};
+
+	type DataEntryMap = {
+		
+	};
+
+	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
 	type ContentConfig = typeof import("../src/content/config");
 }
