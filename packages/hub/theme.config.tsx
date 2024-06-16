b0a22ba1dfd34fe1a427b1563ed60dbb790c926a
ZingerLittleBee <6970999@gmail.com>
2024-06-17 00:16:33 +0800

    docs: ✏️ title

--- packages/hub/theme.config.tsx
+++ packages/hub/theme.config.tsx
@@ -14,6 +14,19 @@ const config: DocsThemeConfig = {
         ServerBee
       </span>
   </>,
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content="ServerBee" />
+            <meta property="og:description" content="ServerBee is an efficient tool that supports monitoring, managing, and controlling multiple platforms, systems, and terminals." />
+        </>
+    ),
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – ServerBee',
+            description: 'ServerBee is an efficient tool that supports monitoring, managing, and controlling multiple platforms, systems, and terminals.',
+        }
+    },
   project: {
     link: 'https://github.com/ZingerLittleBee/server_bee-backend',
   },
@@ -22,7 +35,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/ZingerLittleBee/serverbee-website/tree/main/packages/hub',
   // @ts-ignore
-  footer: false,
+    footer: false,
     i18n: [
         { locale: 'en-US', text: 'English' },
         { locale: 'zh-CN', text: '中文' },
